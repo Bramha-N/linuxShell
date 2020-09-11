cc96dab4c3a71915ae7804862af844d1e3988878
Bramha <69070202+Bramha-N@users.noreply.github.com>
2020-09-12 01:46:45 +0530

    Arithmetic computation problem with solutions

---
+++ d10_arithmetic_computation/UC3-ArithmaticComp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+read -p "Enter the value a : " a
+read -p "Enter the value b : " b
+read -p "Enter the value c : " c
+
+z=$(echo | awk -v B=$b -v C=$c -v A=$a 'BEGIN {print A*B+C}')
+echo "Arithmatic Compute a * b + c = " $z
+
+y=$(echo | awk -v B=$b -v C=$c -v A=$a 'BEGIN {print A+B*C}')
+echo "Arithmatic Compute a + b * c = " $y
