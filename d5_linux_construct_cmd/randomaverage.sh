21678c93c606f4554d7301f5134c3660d1648c58
Bramha <Bramha>
2020-09-12 01:44:19 +0530

    updating

--- d5_linux_construct_cmd/randomaverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-read -p "How many random numbers you want to generate ? : " max
-
-for (( n =1; n<=max; n++ ))
-do
-	y=$(($RANDOM%90+10))
-	echo $y
-done | awk '{sum+= $y} {print sum} END {print sum/NR}'
